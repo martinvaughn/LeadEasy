3bce9e68614d48a12501038dd31043e8b35a7510
Martin Vaughn <59239114+martinvaughn@users.noreply.github.com>
2021-10-07 16:04:44 -0600

    Create InsertLeads.sql

---
+++ DB/InsertLeads.sql
@@ -0,0 +1,73 @@
+USE Leads
+
+
+INSERT INTO Leads
+  (LeadID, Name, Email, Phone, Status, Notes)
+  VALUES (1, 'John James', 'hblee@gmail.com', '480-678-3489', 0, 'He is interested in discussing this with us further. He would like to know how much our services cost and what we can do for him.');
+
+INSERT INTO Leads
+  (LeadID, Name, Email, Phone, Status, Notes)
+  VALUES (2, 'Jeff Holland', 'jholland@gmail.com', '480-678-3489', 2, 'He would like to hear more about our offer and what we bring to the table. However, it sounds like he is on a tight budget for now.');
+
+INSERT INTO Leads
+  (LeadID, Name, Email, Phone, Status, Notes)
+  VALUES (3, 'Abby McGrath', 'abbymcg@gmail.com', '480-678-3489', 0, 'She would like to hear more about our offer and what we bring to the table. However, it sounds like she is on a tight budget for now.');
+
+INSERT INTO Leads
+  (LeadID, Name, Email, Phone, Status, Notes)
+  VALUES (4, 'James Anderson', 'macAndy@gmail.com', '480-678-3489', 1, 'He would like to hear more about our offer and what we bring to the table. However, it sounds like he is on a tight budget for now.');
+
+INSERT INTO Leads
+  (LeadID, Name, Email, Phone, Status, Notes)
+  VALUES (5, 'Mac Anderson', 'macAndy@gmail.com', '480-678-3489', 2, 'He would like to hear more about our offer and what we bring to the table. However, it sounds like he is on a tight budget for now.');
+
+INSERT INTO Leads
+  (LeadID, Name, Email, Phone, Status, Notes)
+  VALUES (6, 'Matt Fukumoto', 'mattfukumoto@gmail.com', '480-678-3489', 1, 'He would like to hear more about our offer and what we bring to the table. However, it sounds like he is on a tight budget for now.');
+
+INSERT INTO Leads
+  (LeadID, Name, Email, Phone, Status, Notes)
+  VALUES (7, 'Riley Price', 'rip@gmail.com', '480-678-3489', 0, 'He would like to hear more about our offer and what we bring to the table. However, it sounds like he is on a tight budget for now.');
+
+INSERT INTO Leads
+  (LeadID, Name, Email, Phone, Status, Notes)
+  VALUES (8, 'Caeden Donaldson', 'Cdon23@gmail.com', '480-678-3489', 0, 'He would like to hear more about our offer and what we bring to the table. However, it sounds like he is on a tight budget for now.');
+
+INSERT INTO Leads
+  (LeadID, Name, Email, Phone, Status, Notes)
+  VALUES (9, 'Jeff Donohue', 'hblee@gmail.com', '480-678-3489', 0, 'He is interested in discussing this with us further. He would like to know how much our services cost and what we can do for him.');
+
+INSERT INTO Leads
+  (LeadID, Name, Email, Phone, Status, Notes)
+  VALUES (10, 'John James', 'hblee@gmail.com', '480-678-3489', 1, 'He is interested in discussing this with us further. He would like to know how much our services cost and what we can do for him.');
+
+INSERT INTO Leads
+  (LeadID, Name, Email, Phone, Status, Notes)
+  VALUES (11, 'Jeff Holland', 'jholland@gmail.com', '480-678-3489', 0, 'He would like to hear more about our offer and what we bring to the table. However, it sounds like he is on a tight budget for now.');
+
+INSERT INTO Leads
+  (LeadID, Name, Email, Phone, Status, Notes)
+  VALUES (12, 'Abby McGrath', 'abbymcg@gmail.com', '480-678-3489', 2, 'She would like to hear more about our offer and what we bring to the table. However, it sounds like she is on a tight budget for now.');
+
+INSERT INTO Leads
+  (LeadID, Name, Email, Phone, Status, Notes)
+  VALUES (13, 'James Anderson', 'macAndy@gmail.com', '480-678-3489', 0, 'He would like to hear more about our offer and what we bring to the table. However, it sounds like he is on a tight budget for now.');
+
+INSERT INTO Leads
+  (LeadID, Name, Email, Phone, Status, Notes)
+  VALUES (14, 'Mac Anderson', 'macAndy@gmail.com', '480-678-3489', 1, 'He would like to hear more about our offer and what we bring to the table. However, it sounds like he is on a tight budget for now.');
+
+INSERT INTO Leads
+  (LeadID, Name, Email, Phone, Status, Notes)
+  VALUES (15, 'Matt Fukumoto', 'mattfukumoto@gmail.com', '480-678-3489', 0, 'He would like to hear more about our offer and what we bring to the table. However, it sounds like he is on a tight budget for now.');
+
+INSERT INTO Leads
+  (LeadID, Name, Email, Phone, Status, Notes)
+  VALUES (16, 'Riley Price', 'rip@gmail.com', '480-678-3489', 0, 'He would like to hear more about our offer and what we bring to the table. However, it sounds like he is on a tight budget for now.');
+
+INSERT INTO Leads
+  (LeadID, Name, Email, Phone, Status, Notes)
+  VALUES (17, 'Caeden Donaldson', 'Cdon23@gmail.com', '480-678-3489', 2, 'He would like to hear more about our offer and what we bring to the table. However, it sounds like he is on a tight budget for now.');
+
+
+SELECT * FROM Leads
